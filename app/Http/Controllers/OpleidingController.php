0a1e888ff2f873c491e4c186b77020f793ccddc0
Bart Roos <bart@studiokaa.co>
2023-02-05 19:13:14 +0100

    Homepage

--- app/Http/Controllers/OpleidingController.php
+++ app/Http/Controllers/OpleidingController.php
@@ -9,7 +9,36 @@ class OpleidingController extends Controller
 {
     public function show(Opleiding $opleiding)
     {
-        return view('opleidingen.show')->with('opleiding', $opleiding);
+        $today = new \Carbon\CarbonImmutable();
+        $maanden_per_blok = 11 / $opleiding->blokken_per_jaar;
+        $dagen = 28 * ($maanden_per_blok - floor($maanden_per_blok));
+        
+        $prev_period = $today->subMonths($maanden_per_blok)->subDays($dagen);
+        if($prev_period->month == 8) $prev_period = $prev_period->subMonth();
+
+        $next_period = $today->addMonths($maanden_per_blok)->subDays($dagen);
+        if($next_period->month == 8) $next_period = $next_period->addMonth();
+
+        $uitvoeren_verleden = $opleiding->uitvoeren()
+                                        ->whereDate('uitvoeren.datum_start', '<=', $prev_period)
+                                        ->whereDate('uitvoeren.datum_eind', '>=', $prev_period)
+                                        ->get();
+
+        $uitvoeren_actueel = $opleiding->uitvoeren()
+                                        ->whereDate('uitvoeren.datum_start', '<=', $today)
+                                        ->whereDate('uitvoeren.datum_eind', '>=', $today)
+                                        ->get();
+
+        $uitvoeren_toekomst = $opleiding->uitvoeren()
+                                        ->whereDate('uitvoeren.datum_start', '<=', $next_period)
+                                        ->whereDate('uitvoeren.datum_eind', '>=', $next_period)
+                                        ->get();
+
+        return view('opleidingen.show')
+            ->with('opleiding', $opleiding)
+            ->with('uitvoeren_verleden', $uitvoeren_verleden)
+            ->with('uitvoeren_actueel', $uitvoeren_actueel)
+            ->with('uitvoeren_toekomst', $uitvoeren_toekomst);
     }
 
     public function edit(Opleiding $opleiding)
--- resources/views/opleidingen/show.blade.php
+++ resources/views/opleidingen/show.blade.php
@@ -1,8 +1,53 @@
 @extends('layouts.app')
 
 @section('main')
-
-    Dit scherm toont straks alle uitvoeren die _dit schooljaar_ lopen.
     
+    <div style="display: grid; grid-template-columns: repeat(3, 1fr); row-gap: 1rem; column-gap: 3rem;">
+
+        <h3 style="grid-row: 1; grid-column: 1;">Afgelopen blokken</h3>
+        @foreach ($uitvoeren_verleden as $uitvoer)
+            <div class="card hover-show" style="grid-row: {{ $loop->iteration+1 }}; grid-column: 1;">
+                <div class="card-body d-flex justify-content-between align-items-center">
+                    <h5 class="card-title">{{ $uitvoer->naam }}</h5>
+                    <a class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-eye fa-fw"></i></a>
+                </div>
+                <ul class="list-group list-group-flush">
+                    @foreach($uitvoer->cohorten as $cohort)
+                        <li class="list-group-item">{{ $cohort->naam }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endforeach
+
+        <h3 style="grid-row: 1; grid-column: 2;">Actuele blokken</h3>
+        @foreach ($uitvoeren_actueel as $uitvoer)
+            <div class="card hover-show border-secondary bg-light" style="grid-row: {{ $loop->iteration+1 }}; grid-column: 2;">
+                <div class="card-body d-flex justify-content-between align-items-center">
+                    <h5 class="card-title">{{ $uitvoer->naam }}</h5>
+                    <a class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-eye fa-fw"></i></a>
+                </div>
+                <ul class="list-group list-group-flush">
+                    @foreach($uitvoer->cohorten as $cohort)
+                        <li class="list-group-item">{{ $cohort->naam }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endforeach
+
+        <h3 style="grid-row: 1; grid-column: 3;">Aankomende blokken</h3>
+        @foreach ($uitvoeren_toekomst as $uitvoer)
+            <div class="card hover-show" style="grid-row: {{ $loop->iteration+1 }}; grid-column: 3;">
+                <div class="card-body d-flex justify-content-between align-items-center">
+                    <h5 class="card-title">{{ $uitvoer->naam }}</h5>
+                    <a class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-eye fa-fw"></i></a>
+                </div>
+                <ul class="list-group list-group-flush">
+                    @foreach($uitvoer->cohorten as $cohort)
+                        <li class="list-group-item">{{ $cohort->naam }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endforeach
+    </div>
 
 @endsection
