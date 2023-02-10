776d2fe450bc0f5c6dcfdcbc0dd6f0fccf2741e9
Bart Roos <bart@studiokaa.co>
2023-02-10 23:30:36 +0100

    Maak leerplan printbaar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         @livewireStyles
     </head>
     <body>
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark d-print-none">
             <div class="container-fluid">
                 <a class="navbar-brand" href="{{ route('home') }}">CurrApp</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- resources/views/uitvoeren/show.blade.php
+++ resources/views/uitvoeren/show.blade.php
@@ -6,7 +6,7 @@
     <nav class="navbar navbar-dark bg-secondary subnav">
         <div class="container-fluid">
             <div class="navbar-brand">{{ $uitvoer->naam }}</div>
-            <div>
+            <div class="d-print-none">
                 <button class="btn btn-outline-light me-2" data-bs-toggle="modal" data-bs-target="#linkModuleModal"><i class="fa-solid fa-plus fa-fw"></i> Module</button>
                 <button class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#linkVakModal"><i class="fa-regular fa-edit fa-fw"></i> Vakken</button>
             </div>
@@ -25,7 +25,7 @@
         @foreach($uitvoer->vakken as $vak)
             <div style="grid-column: {{ $loop->iteration+1 }}; grid-row: 1; text-align: center;"><strong>{{ $vak->parent->naam }}</strong></div>
             @foreach ($vak->modules as $module)
-                <div class="module p-2 text-center" style="grid-column: {{ $loop->parent->iteration+1 }}; grid-row: {{ $module->pivot->week_start+1 }} / {{ $module->pivot->week_eind+2 }};">
+                <div class="module p-2 text-center" style="background-color: {{ $module->parent->leerlijn->color }}; grid-column: {{ $loop->parent->iteration+1 }}; grid-row: {{ $module->pivot->week_start+1 }} / {{ $module->pivot->week_eind+2 }};">
                     {{ $module->parent->naam }} {{ $module->naam }}
                 </div>
             @endforeach
