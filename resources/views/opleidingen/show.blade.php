331bd4611f22d82bc447cd83d88b52f11ab90511
Bart Roos <bart@studiokaa.co>
2023-11-02 16:41:55 +0100

    Fix homepage

--- resources/views/opleidingen/show.blade.php
+++ resources/views/opleidingen/show.blade.php
@@ -2,28 +2,33 @@
 
 @section('main')
     
-    <div class="mb-3" style="display: grid; grid-template-columns: repeat(3, 1fr); row-gap: 1rem; column-gap: 3rem;">
+    <div class="mb-3" style="display: grid; grid-template-columns: repeat(3, 1fr); column-gap: 3rem;">
+        <div>
+            <h3>Afgelopen blokken</h3>
+            @foreach ($uitvoeren_verleden as $uitvoer)
+                <div class="card hover-show mb-3">
+                    @include('opleidingen.card_contents', ['uitvoer' => $uitvoer])
+                </div>
+            @endforeach
+        </div>
 
-        <h3 style="grid-row: 1; grid-column: 1;">Afgelopen blokken</h3>
-        @foreach ($uitvoeren_verleden as $uitvoer)
-            <div class="card hover-show" style="grid-row: {{ $loop->iteration+1 }}; grid-column: 1;">
-                @include('opleidingen.card_contents', ['uitvoer' => $uitvoer])
-            </div>
-        @endforeach
+        <div>
+            <h3>Actuele blokken</h3>
+            @foreach ($uitvoeren_actueel as $uitvoer)
+                <div class="card hover-show border-secondary bg-light mb-3">
+                    @include('opleidingen.card_contents', ['uitvoer' => $uitvoer])
+                </div>
+            @endforeach
+        </div>
 
-        <h3 style="grid-row: 1; grid-column: 2;">Actuele blokken</h3>
-        @foreach ($uitvoeren_actueel as $uitvoer)
-            <div class="card hover-show border-secondary bg-light" style="grid-row: {{ $loop->iteration+1 }}; grid-column: 2;">
-                @include('opleidingen.card_contents', ['uitvoer' => $uitvoer])
-            </div>
-        @endforeach
-
-        <h3 style="grid-row: 1; grid-column: 3;">Aankomende blokken</h3>
-        @foreach ($uitvoeren_toekomst as $uitvoer)
-            <div class="card hover-show" style="grid-row: {{ $loop->iteration+1 }}; grid-column: 3;">
-                @include('opleidingen.card_contents', ['uitvoer' => $uitvoer])
-            </div>
-        @endforeach
+        <div>
+            <h3>Aankomende blokken</h3>
+            @foreach ($uitvoeren_toekomst as $uitvoer)
+                <div class="card hover-show mb-3">
+                    @include('opleidingen.card_contents', ['uitvoer' => $uitvoer])
+                </div>
+            @endforeach
+        </div>
     </div>
 
 @endsection
