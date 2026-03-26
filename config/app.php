7c26ddd790a110bcbe60f013f4feaf7908fd99c8
Shift <shift@laravelshift.com>
2026-03-26 11:07:09 +0000

    Shift cleanup

--- config/app.php
+++ config/app.php
@@ -1,14 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Facade;
-use Illuminate\Support\ServiceProvider;
 
 return [
 
     'weeks' => [
         'api_url' => env('WEEKS_API_URL', 'https://week.curio.codes/api/'),
     ],
-
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
