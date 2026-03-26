16a4c27221aceaf68b596dd4d26d08671a2752c9
Shift <shift@laravelshift.com>
2026-03-26 11:07:04 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -9,20 +9,6 @@ return [
         'api_url' => env('WEEKS_API_URL', 'https://week.curio.codes/api/'),
     ],
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
